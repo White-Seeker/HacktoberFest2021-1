f2a10a6766670cd97b8a6836500cc4eeafd660ff
ApplePie <65601844+White-Seeker@users.noreply.github.com>
2024-05-22 16:44:39 +0530

    Delete Django_portfolio_application/portfolio directory

--- Django_portfolio_application/portfolio/README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Application using Django Web Framework
-
-My portfolio can be found on the following link: https://aditya26sg.pythonanywhere.com/
--- Django_portfolio_application/portfolio/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('personalprojectapi', views.PersonalProjectView.as_view(), name='personalprojectApi'),
-    path('personalworkapi',views.PersonalWorkView.as_view(), name='personalworkApi')
-]
