7872101f57977eca440ae2d7b2cc9f56a7a11a69
ApplePie <65601844+White-Seeker@users.noreply.github.com>
2024-05-22 16:45:47 +0530

    Delete Java directory

--- Java/Matrix Calculator/README.md
+++
@@ -1,3 +0,0 @@
-# MATRIX Calculator using Java-8
-
-## Matrix_CALC.java and Matrix_MOD.java both are abstract classes and the main method is defined in the Caller.java
--- Java/a.java
+++
@@ -1,9 +0,0 @@
-public class TestString {
-    public static void main(String[] args) {
-  String x = "123";
-      String y = "XYZ";
-     x.concat(y);
-      System.out.print(x);
-         }
-      }
-      
